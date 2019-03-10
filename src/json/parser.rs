0e6376a503bd708bf69629ad71fc52b17c4c77e4
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-03-10 19:58:04 +0100

    json/parser: add tests

--- src/json/parser.rs
+++ src/json/parser.rs
@@ -90,3 +90,153 @@ pub fn parse_json_lines(json: serde_json::Value, indent: usize) -> Result<Vec<Js
 
     Ok(lines)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::json::{JsonLine, JsonToken};
+
+    #[test]
+    fn test_parse_json_primitive() {
+        let input_json = r##"{
+    "empty-array": [],
+    "empty-object": {},
+    "name": "mattors",
+    "ref1": "#/ciaomondo/23",
+    "private": true
+}
+"##;
+
+        let expected = vec![
+            JsonLine::new(vec![JsonToken::object_start()]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("empty-array".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::array_start(),
+                JsonToken::array_end(),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("empty-object".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::object_start(),
+                JsonToken::object_end(),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("name".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::string("mattors".to_string()).unwrap(),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("private".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::bool(true),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("ref1".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::string("#/ciaomondo/23".to_string()).unwrap(),
+            ]),
+            JsonLine::new(vec![JsonToken::object_end()]),
+        ];
+
+        let value = serde_json::from_str(input_json).unwrap();
+        let lines = super::parse_json_lines(value, 0).unwrap();
+
+        assert_eq!(lines.len(), expected.len());
+        for (i, (g, e)) in lines.into_iter().zip(expected.into_iter()).enumerate() {
+            assert_eq!(g, e, "line #{} differ", i);
+        }
+    }
+
+    #[test]
+    fn test_parse_simple_array() {
+        let input_json = "[1,null,true]";
+
+        let expected = vec![
+            JsonLine::new(vec![JsonToken::array_start()]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::number(1.into()),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::null(),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![JsonToken::ws(4), JsonToken::bool(true)]),
+            JsonLine::new(vec![JsonToken::array_end()]),
+        ];
+
+        let value = serde_json::from_str(input_json).unwrap();
+        let lines = super::parse_json_lines(value, 0).unwrap();
+
+        assert_eq!(lines.len(), expected.len());
+        for (i, (g, e)) in lines.into_iter().zip(expected.into_iter()).enumerate() {
+            assert_eq!(g, e, "line #{} differ", i);
+        }
+    }
+
+    #[test]
+    fn test_parse_nested_json() {
+        let input_json = r##"{"a" : [1,2,3, {"hello-world": null}]}"##;
+
+        let expected = vec![
+            JsonLine::new(vec![JsonToken::object_start()]),
+            JsonLine::new(vec![
+                JsonToken::ws(4),
+                JsonToken::object_key("a".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::array_start(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(8),
+                JsonToken::number(1.into()),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(8),
+                JsonToken::number(2.into()),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![
+                JsonToken::ws(8),
+                JsonToken::number(3.into()),
+                JsonToken::comma(),
+            ]),
+            JsonLine::new(vec![JsonToken::ws(8), JsonToken::object_start()]),
+            JsonLine::new(vec![
+                JsonToken::ws(12),
+                JsonToken::object_key("hello-world".to_string()).unwrap(),
+                JsonToken::colon(),
+                JsonToken::ws(1),
+                JsonToken::null(),
+            ]),
+            JsonLine::new(vec![JsonToken::ws(8), JsonToken::object_end()]),
+            JsonLine::new(vec![JsonToken::ws(4), JsonToken::array_end()]),
+            JsonLine::new(vec![JsonToken::object_end()]),
+        ];
+
+        let value = serde_json::from_str(input_json).unwrap();
+        let lines = super::parse_json_lines(value, 0).unwrap();
+
+        assert_eq!(lines.len(), expected.len());
+        for (i, (g, e)) in lines.into_iter().zip(expected.into_iter()).enumerate() {
+            assert_eq!(g, e, "line #{} differ", i);
+        }
+    }
+}
